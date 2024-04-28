bd3085a113743d3634e939131b7f3dafb8c8bf63
hyh <2873258684@qq.com>
2024-04-28 13:48:40 +0800

    v0.2根目录联想@设置

--- smallRabbit/src/main.js
+++ smallRabbit/src/main.js
@@ -1,11 +1,22 @@
 import { createApp } from 'vue'
 import{createPinia} from "pinia"
+import { createRouter,createWebHashHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
 
+const route=[
+    
+]
+
+
+
+const router=createRouter({
+  route:route,
+  history:createWebHashHistory()
+})
 const pinia=createPinia()
 
 const app=createApp(App)
-
+app.use(router)
 app.use(pinia)
 app.mount('#app')
