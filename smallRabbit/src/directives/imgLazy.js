54c77f39ef7c72835b430ad5e26a765c0eb798fb
hyh <2873258684@qq.com>
2024-04-30 20:44:04 +0800

    v0.36 home hot 插件实现图片懒加载（优化）

---
+++ smallRabbit/src/directives/imgLazy.js
@@ -0,0 +1,22 @@
+
+import { useIntersectionObserver } from '@vueuse/core'
+
+export const imgLazy={
+install(app){
+    app.directive('img-lazy',{
+        mounted(el,binding){
+           useIntersectionObserver(
+            el,
+            ([{ isIntersecting }]) => {
+             if(isIntersecting ){
+              el.src=binding.value
+             }
+            },
+          )
+        }
+        })
+
+
+}
+
+}
--- smallRabbit/src/main.js
+++ smallRabbit/src/main.js
@@ -3,21 +3,10 @@ import { createPinia } from 'pinia'
 import router from '@/router/index.js'
 import './styles/common.scss'
 import App from './App.vue'
-import { useIntersectionObserver } from '@vueuse/core'
+import {imgLazy} from '@/directives/imgLazy';
 const pinia=createPinia()
 const app = createApp(App)
-app.directive('img-lazy',{
-mounted(el,binding){
-   useIntersectionObserver(
-    el,
-    ([{ isIntersecting }]) => {
-     if(isIntersecting ){
-      el.src=binding.value
-     }
-    },
-  )
-}
-})
+app.use(imgLazy)
 app.use(pinia)
 app.use(router)
 app.mount('#app')
