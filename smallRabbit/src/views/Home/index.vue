3530f0aa3283fc74efefd59966b40944c32a75c0
hyh <2873258684@qq.com>
2024-04-30 13:54:07 +0800

    v0.29 Layout吸顶导航

--- smallRabbit/src/views/Home/index.vue
+++ smallRabbit/src/views/Home/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 </script>
 <template>
+    <div style="height: 1000px;">啊大大</div>
     我是home
 </template>
 <style>
--- smallRabbit/src/views/Layout/index.vue
+++ smallRabbit/src/views/Layout/index.vue
@@ -2,11 +2,11 @@
 import LayoutNav from '@/views/Layout/components/LayoutNav.vue'
 import LayoutHeader from '@/views/Layout/components/LayoutHeader.vue'
 import LayoutFooter from '@/views/Layout/components/LayoutFooter.vue'
-
+import LayoutFixed from './components/LayoutFixed.vue';
 
 </script>
 <template>
-    
+ <layout-fixed></layout-fixed>   
 <layout-nav></layout-nav>
 <layout-header></layout-header>
 <router-view></router-view> 
