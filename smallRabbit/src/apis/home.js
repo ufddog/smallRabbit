cb8b57324d114f48dcd8643107e3e0584769aecc
hyh <2873258684@qq.com>
2024-05-01 17:52:26 +0800

    0.42 分类轮播图实现

--- smallRabbit/src/apis/home.js
+++ smallRabbit/src/apis/home.js
@@ -1,9 +1,12 @@
 import httpInstance from '@/utils/http.js' //有{}为引入按需导出 无{}为引入默认导出
 
-export function getBannerApi(){
-
+export function getBannerApi(res={}){
+   const{distributionSite='1'}=res
     return httpInstance({
-     url:'home/banner'
+     url:'home/banner',
+     params:{
+      distributionSite
+     }
 
     })
 }
--- smallRabbit/src/views/Category/index.vue
+++ smallRabbit/src/views/Category/index.vue
@@ -2,6 +2,15 @@
 import {onMounted, onUpdated, ref} from 'vue'
 import {getCategoryApi} from '@/apis/category'
 import { useRoute } from 'vue-router';
+import {getBannerApi} from '@/apis/home.js'
+const bannerList=ref([])
+const getBanner=async ()=>{
+
+ const res=await getBannerApi({distributionSite:'2'})
+ console.log(res);
+ bannerList.value=res.result
+
+}
 const route=useRoute()
 const categoryList=ref({})
 const getCategory= async ()=>{
@@ -11,7 +20,8 @@ categoryList.value=res.result
 }
 
 onMounted(()=>{
-getCategory()})
+getCategory()
+getBanner()})
 
 </script>
 
@@ -25,8 +35,18 @@ getCategory()})
           <el-breadcrumb-item>{{ categoryList.name }}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
+      <div class="home-banner">
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in bannerList" :key="item">
+        <img :src="item.imgUrl" alt="">
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+
     </div>
   </div>
+ 
+
 </template>
 
 
@@ -106,6 +126,15 @@ getCategory()})
 
   .bread-container {
     padding: 25px 0;
+  }
+}
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  margin: auto;
+  img {
+    width: 100%;
+    height: 500px;
   }
 }
 </style>
