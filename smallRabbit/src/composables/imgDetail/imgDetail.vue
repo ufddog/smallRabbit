88616a63534d0afdc3464c2479f63ca575cac5c6
hyh <2873258684@qq.com>
2024-05-03 14:59:06 +0800

    0.55 detail 放大镜效果 滑块跟随鼠标

--- smallRabbit/src/composables/imgDetail/imgDetail.vue
+++ smallRabbit/src/composables/imgDetail/imgDetail.vue
@@ -1,5 +1,6 @@
 <script setup>
-import {ref} from 'vue'
+import {ref, watch} from 'vue'
+import { useMouseInElement } from '@vueuse/core';
 // 图片列表
 const imageList = [
   "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
@@ -13,18 +14,46 @@ function mouseEnterImg(i){
 idexImg.value=i
 
 }
+const target = ref(null)
 
+const { elementX, elementY, isOutside } = useMouseInElement(target)
+//elementX, elementY为鼠标在dom元素内与页面的相对位置 x,y为鼠标在页面的位置
+const left=ref(0)
+const top=ref(0)
+
+watch([elementX,elementY],()=>{
+
+if(elementX.value<300&&elementX.value>100){
+  left.value=elementX.value-100
+}
+if(elementY.value<300&&elementY.value>100){
+  top.value=elementY.value-100
+}
+if(elementX.value>=300){
+ left.value=200
+}
+if(elementX.value<=100){
+ left.value=0
+}
+if(elementY.value>=300){
+ top.value=200
+}
+if(elementY.value<=100){
+ top.value=0
+}
+})
 
 </script>
 
 
 <template>
+  
   <div class="goods-image">
     <!-- 左侧大图-->
     <div class="middle" ref="target">
       <img :src="imageList[idexImg]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div class="layer" :style="{ left:`${left}px`, top:`${top}px` }" ></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
@@ -35,7 +64,7 @@ idexImg.value=i
     <!-- 放大镜大图 -->
     <div class="large" :style="[
       {
-        backgroundImage: `url(${imageList[0]})`,
+        backgroundImage: `url(${imageList[idexImg]})`,
         backgroundPositionX: `0px`,
         backgroundPositionY: `0px`,
       },
